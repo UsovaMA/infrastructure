5fdaa850561841d40e2250cd41fa87ff326ec9da
Usova Marina A <oppabang@mail.ru>
2017-04-30 23:15:01 +0300

    Fix cpplint

--- src/islands.cpp
+++ src/islands.cpp
@@ -93,7 +93,6 @@ std::vector<std::vector<int>> PercolationForDS
 /* Обход в ширину (breadth-first search) */
 void BFS(std::vector<std::vector<int>> matrix,
   std::vector<std::vector<int>> *field, index Y) {
-
   int matrix_size = matrix.size();
   std::queue<index> q;
   q.push(Y);
